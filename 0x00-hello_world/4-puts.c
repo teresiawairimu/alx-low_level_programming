7782e65fc50f919294e12216bc5b6c12bd2728ec
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-15 12:21:18 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str1);
 	return (0);
