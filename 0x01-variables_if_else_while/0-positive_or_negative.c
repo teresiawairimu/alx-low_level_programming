f0ef7b38e3497486310c34820492417a688428fe
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-14 19:52:42 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,14 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'assign a random number to the variable n'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
