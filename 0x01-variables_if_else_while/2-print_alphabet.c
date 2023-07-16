736d85ce78842af90b1d182105fba9b623f1b51e
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-16 07:55:04 -0700

    2-print_alpabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - prints the alphabet in lowercase
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
