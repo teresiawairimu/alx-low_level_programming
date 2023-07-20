bb9f8d666f31e294a56c79368735b5734527b699
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-19 18:25:48 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: function parameter that is used to pass a value from the calling code
+ * @num2: function parameter that is used to pass a value from the calling code
+ * Return: the result
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
