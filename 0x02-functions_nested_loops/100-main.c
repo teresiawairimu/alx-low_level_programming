423c1618a89de016394a6abfbf5260289e0cfac4
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-20 18:56:17 -0700

    changes

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_tables(5);
+	_putchar('\n');
+	print_times_tables(98);
+	_putchar('\n');
+	print_times_tables(12);
+	return (0);
+}
