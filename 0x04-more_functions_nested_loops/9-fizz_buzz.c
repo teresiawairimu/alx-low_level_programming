a6456d42e8fd389631f1d4358582263bd6c26d96
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-23 03:41:42 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,21 +17,13 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 15 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (n % 3 == 0)
-		{
 			printf("Fizz ");
-		}
 		else if (n % 5 == 0)
-		{
 			printf("Buzz ");
-		}
 		else
-		{
 			printf("%d ", n);
-		}
 	}
 	printf("\n");
 	return (0);
