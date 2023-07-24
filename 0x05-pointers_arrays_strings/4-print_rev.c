e975c9e8cff86809922b249e41d7a5849d87017c
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-24 09:50:37 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse 
+ * followed by a new line
+ * @s: function parameter
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+	int m;
+	
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	
+	for (m = n-1; m >= 0; m--)
+	{
+		_putchar(s[m]);
+	}
+	_putchar('\n');
+}
