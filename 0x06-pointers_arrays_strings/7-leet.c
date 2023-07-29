ee11f9f18d9a7f9d04703aa2411b46c78002617b
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-28 17:58:04 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,20 +12,15 @@ char *leet(char *str)
 	char n[] = "aAeEoOtT1L";
 	char m[] = "4433007711";
 
-	count1 = 0;
-	while (str[count1] != '\0')
+	for (count1 = 0; str[count1] != '\0'; count1++)
 	{
-		count2 = 0;
-		while (count2 < 10)
+		for (count2 = 0; count2 < 10; count2++)
 		{
 			if (n[count2] == str[count1])
 			{
 				str[count1] = m[count2];
-				break;
 			}
-			count2++;
 		}
-		count1++;
 	}
 	return (str);
 }
