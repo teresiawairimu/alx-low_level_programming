deb4f3871289c9aa3cf1c815377a287d87992fcc
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-31 16:50:09 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string
+ * @accept: function parameter
+ *
+ * Return: the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
