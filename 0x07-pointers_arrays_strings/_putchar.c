80d14c7d8f1cdae634a2bfbb45396b80d465c89d
teresiawairimu <teresiawairimu06@gmail.com>
2023-07-31 18:49:52 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes c character to standard output
+ *
+ * Return: 1 (success)
+ * on error return -1
+ */
+int _putchar(int c)
+{
+	return(write(1, &c, 1));
+}
