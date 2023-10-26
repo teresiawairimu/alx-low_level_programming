781002a8cbf7c5884183237b531270a2e0548752
teresiawairimu <nduatiteresia82@gmail.com>
2023-10-25 19:59:00 -0700

    task 8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,5 +33,22 @@ int is_palindrome(char *s)
 		return (1);
 	return (check_palindrome(s, 0, length - 1));
 }
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string whose length is returned
+ *
+ * Return: integer value that indicates the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+        int length = 0;
+
+        if (*s != '\0')
+        {
+                length = 1 + _strlen_recursion(s + 1);
+        }
+        return (length);
+}
+
 
 	
