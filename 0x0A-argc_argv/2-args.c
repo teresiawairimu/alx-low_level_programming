ac150e9de4c89907b6c88bd6e7e1ffd5210e066b
teresiawairimu <nduatiteresia82@gmail.com>
2023-10-27 12:42:04 -0700

    task 3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,13 +3,14 @@
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments passed to the program
- * @argv: one-dimensional array of strings 
+ * @argv: one-dimensional array of strings
  *
  * Return: all arguments, one argument per line
  */
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
