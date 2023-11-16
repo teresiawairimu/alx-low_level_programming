161dac989362d2243d2810c8814928c8a70eabb4
teresiawairimu <nduatiteresia82@gmail.com>
2023-11-16 13:03:22 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,48 +1,26 @@
 #include "lists.h"
-
 /**
- * add_node - Inserts a new node at the beginning of a list
- * @head: Pointer to the head of the list
- * @str: String to be added
- *
- * Return: Returns the address of the new element or NULL if failed
+ * add_node - Inserts a new node at the beginning of a singly linked list
+ * @head: Double pointer to the list_t list
+ * @str: New string to be added in the node
+ * Return: Address of the new element, or NULL if the operation fails
  */
-
-/*
-1. Allocate memory for a new node.
-2. If the memory allocation fails, return NULL.
-3. Copy the string into a new buffer.
-4. If the string copy fails, free the new node and return NULL.
-5. Calculate the length of the string.
-6. Set the new node’s next pointer to point to the current head of the list.
-7. Update the head of the list to point to the new node.
-8. Return a pointer to the new node.
-*/
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
 	list_t *new;
+	unsigned int len = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (len = 0; str[len];)
+	while (str[len])
 		len++;
 
-	new->str = dup;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
 	new->len = len;
-	new->next = *head;
+	new->next = (*head);
+	(*head) = new;
 
-	*head = new;
-
-	return (new);
-
+	return (*head);
 }
