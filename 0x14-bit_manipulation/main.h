b89fff432d0334613369d2ddffdb6fef6c944364
teresiawairimu <nduatiteresia82@gmail.com>
2023-11-23 10:02:33 -0800

    Added the main.h file with the prototypes

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
+
+#include <unistd.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif /* #ifndef BIT_MANIPULATION_H_ */
