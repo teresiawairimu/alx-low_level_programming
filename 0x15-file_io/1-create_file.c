cbeee9624c39e63c5adb83b84b7141ac6a1ca3c7
teresiawairimu <nduatiteresia82@gmail.com>
2023-11-27 14:41:10 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,50 @@
 #include "main.h"
+
 /**
- * create_file - Creates a file with the given name.
- * @filename: Name of the file.
- * @text_content: NULL-terminated string to write to the file.
- * Return: 1 on success, -1 on failure (unable to create/write file).
+ * _strlen - Returns the length of a string.
+ * @s: Input character array.
+ * Return: Length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * create_file - Creates a file with specified content.
+ * @filename: File to create.
+ * @text_content: Information to write into the file.
+ * Return: 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	register int _file, _write, text_len = 0;
+	ssize_t n_letters;
+	int file;
 
-	if (!(filename))
-	{
-		return (-1);
-	}
-	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (_file == -1)
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 	{
 		return (-1);
 	}
 	if (text_content)
 	{
-		while (text_content[text_len])
+		n_letters = write(file, text_content, _strlen(text_content));
+		if (n_letters == -1)
 		{
-			text_len++;
-		}
-		_write = write(_file, text_content, text_len);
-		if (_write == -1)
-		{
+			close(file);
 			return (-1);
 		}
 	}
-	close(_file);
+	close(file);
 	return (1);
 }
