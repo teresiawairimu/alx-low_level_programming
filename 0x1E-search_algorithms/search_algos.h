bb85b737bd39ddbe6efb1c3a7871511da72e5902
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-17 10:09:27 -0700

    tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_search_array(int *array, size_t left, size_t right);
+int jump_search(int *array, size_t size, int value);
 #endif
